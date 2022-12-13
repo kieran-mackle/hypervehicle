126f4144b551d805cd1bc50f482ebf90628c2213
Kieran <kemackle98@gmail.com>
2022-12-14 09:48:09 +1000

    ci: added cz bumping to ci workflow

--- hypervehicle/__init__.py
+++ hypervehicle/__init__.py
@@ -1,2 +1,5 @@
 from .vehicle import Vehicle
 from .polygon_formation import Polygon_formation
+
+
+__version__ = "0.1.0"
--- setup.py
+++ setup.py
@@ -1,11 +1,33 @@
-from setuptools import setup, find_packages
+import codecs
+import os.path
+import setuptools
 
-setup(
+
+def read(rel_path):
+    here = os.path.abspath(os.path.dirname(__file__))
+    with codecs.open(os.path.join(here, rel_path), "r") as fp:
+        return fp.read()
+
+
+def get_version(rel_path):
+    for line in read(rel_path).splitlines():
+        if line.startswith("__version__"):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    else:
+        raise RuntimeError("Unable to find version string.")
+
+
+# Load README
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
     name="hypervehicle",
-    version="0.1.0",
+    version=get_version("hypervehicle/__init__.py"),
     author="Ingo Jahn, Kieran Mackle",
     author_email="ingo.jahn@usq.edu.au, k.mackle@uq.net.au",
-    packages=find_packages(),
+    packages=setuptools.find_packages(),
     url="https://github.com/kieran-mackle/hypervehicle",
     description="Parametric geometry generation tool for hypersonic vehicles",
     long_description=open("README.md").read(),
