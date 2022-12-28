1d7604b1640b45c30f2e5b8f841d6e998e88a9ab
Kieran <kemackle98@gmail.com>
2022-12-29 07:05:14 +1000

    feat(common.py): added uniform thickness function generator

--- hypervehicle/components/common.py
+++ hypervehicle/components/common.py
@@ -11,3 +11,13 @@ def leading_edge_width_function(r):
     )
     le_width = temp(r).y
     return le_width
+
+
+def uniform_thickness_function(thickness: float, side: str):
+    """Returns a function handle."""
+    m = -1 if side == "top" else 1
+
+    def tf(x: float, y: float, z: float = 0):
+        return Vector3(x=x, y=y, z=m * thickness / 2)
+
+    return tf
