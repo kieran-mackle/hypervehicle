c31a1f2b79b59e9468c62c2335000a48481d8b5e
Kieran <kemackle98@gmail.com>
2023-01-17 11:19:50 +1000

    feat(Generator): implemented base generator class

--- hypervehicle/generator.py
+++ hypervehicle/generator.py
@@ -10,3 +10,10 @@ class AbstractGenerator(ABC):
     @abstractmethod
     def create_instance(self) -> Vehicle:
         pass
+
+
+class Generator(AbstractGenerator):
+    def __init__(self, **kwargs) -> None:
+        # Unpack kwargs and overwrite parameter named attributes
+        for item in kwargs:
+            setattr(self, item, kwargs[item])
