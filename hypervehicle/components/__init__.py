9331ae0fa5bdf435ca38cb8fc4b6d722ad0b5c9e
Kieran <kemackle98@gmail.com>
2023-02-03 07:43:33 +1000

    feat(polygon.py): updated polygon to standard component type

--- hypervehicle/components/__init__.py
+++ hypervehicle/components/__init__.py
@@ -2,6 +2,6 @@ from .fin import Fin
 from .wing import Wing
 from .component import Component
 from .swept import SweptComponent
-from .polygon import poly_gen_main
+from .polygon import Cube, Sphere
 from .revolved import RevolvedComponent
 from .composite import CompositeComponent
