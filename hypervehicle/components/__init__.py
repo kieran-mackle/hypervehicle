df3483c0c833a857e6557d2b66a01dd0f5b21fff
Kieran <kemackle98@gmail.com>
2023-04-03 07:15:08 +1000

    refactor(special.py): renamed polygon to special components
    added starting code for roughness hexahedron component

--- hypervehicle/components/__init__.py
+++ hypervehicle/components/__init__.py
@@ -2,6 +2,6 @@ from .fin import Fin
 from .wing import Wing
 from .component import Component
 from .swept import SweptComponent
-from .polygon import Cube, Sphere
 from .revolved import RevolvedComponent
 from .composite import CompositeComponent
+from .special import Cube, Sphere, RoughHexahedron
--- hypervehicle/components/special.py
+++ hypervehicle/components/special.py
@@ -1,6 +1,13 @@
+from copy import deepcopy
 from hypervehicle.components import Component
-from hypervehicle.components.constants import CUBE, SPHERE
-from hypervehicle.geometry import Vector3, CubePatch, SpherePatch
+from hypervehicle.components.constants import CUBE, SPHERE, HEXAHEDRON
+from hypervehicle.geometry import (
+    Vector3,
+    CubePatch,
+    SpherePatch,
+    RoughnessPatch,
+    RoughnessElement,
+)
 
 
 class Cube(Component):
@@ -60,3 +67,37 @@ class Sphere(Component):
 
         for face in faces:
             self.patches[face] = SpherePatch(self.r, self.centre, face)
+
+
+class RoughHexahedron(Component):
+    componenttype = HEXAHEDRON
+
+    def __init__(
+        self,
+        params: dict = None,
+        stl_resolution: int = 2,
+        verbosity: int = 1,
+        name: str = None,
+    ) -> None:
+        """Create a hexehedron with rough surface."""
+
+        super().__init__(stl_resolution=stl_resolution, verbosity=verbosity, name=name)
+
+    def generate_patches(self):
+        # Generate nominal cube
+        cube = Cube()
+        cube.generate_patches()
+
+        patches = deepcopy(cube.patches)
+
+        # Generate nominal roughness patch
+        roughness_element = RoughnessElement(
+            80e-3, 80e-3, 0.1, 40e-3, Nx=0, Ny=0, data=None, ramp_x=0.1, ramp_y=0.1
+        )
+        rp = RoughnessPatch(roughness_element, x_range, y_range)
+
+        # Replace top surface with roughness patch
+        patches.pop("north")
+        patches["north"] = rp
+
+        self.patches = patches
